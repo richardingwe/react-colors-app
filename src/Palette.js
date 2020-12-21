a0d80141f5e7c8b33403e332c1595e2ac4de6da2
Richard Ingwe <richardingwe5@gmail.com>
2020-12-21 13:05:28 +0100

    created color palette component

---
+++ src/Palette.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class Palette extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello</h1>
+                <div className="Palette">
+                    {/* navbar goes here */}
+                    <div className="Palette-colors">{/* palete colors goes here */}</div>
+                    {/* footer goes here */}
+                </div>
+            </div>
+        )
+    }
+}
