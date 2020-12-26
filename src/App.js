19c903b459fafafd724a58ac697e641db56b44a5
Richard Ingwe <richardingwe5@gmail.com>
2020-12-26 19:58:56 +0100

    add new palette form and link to palette form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Palette from './Palette';
 import PaletteList from "./PaletteList";
 import SingleColorPalette from "./SingleColorPalette";
 import seedColors from './seedColors';
+import NewPaletteForm from "./NewPaletteForm";
 import { generatePalette } from './colorHelpers';
 import './App.css';
 
@@ -14,6 +15,11 @@ function App() {
   };
   return (
     <Switch>
+      <Route
+        exact
+        path="/palette/new"
+        render={() => <NewPaletteForm />}
+      />
       <Route
         exact
         path="/palette/:paletteId/:colorId"
