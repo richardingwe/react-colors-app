60cd7ebf06e31703a2cb30ed3797263114c89d91
Richard Ingwe <richardingwe5@gmail.com>
2020-12-30 14:44:18 +0100

    tweaked styles of form

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -8,7 +8,7 @@ import Typography from '@material-ui/core/Typography';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Button from '@material-ui/core/Button';
-import MenuIcon from '@material-ui/icons/Menu';
+import AddToPhotosicon from '@material-ui/icons/AddToPhotos';
 import useStyles from "./styles/PaletteFormNavStyles";
 
 
@@ -41,7 +41,7 @@ export default function PaletteFormNav(props) {
                         edge="start"
                         className={clsx(classes.menuButton, open && classes.hide)}
                     >
-                        <MenuIcon />
+                        <AddToPhotosicon />
                     </IconButton>
                     <Typography variant="h6" noWrap>
                         Create A Palette
