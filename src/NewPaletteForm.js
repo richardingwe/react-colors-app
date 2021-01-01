5acad781f35a1a0ca6e579a8eab6ac8e7a979e8e
Richard Ingwe <richardingwe5@gmail.com>
2021-01-01 02:43:42 +0100

    fixed deletedrag bug

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -133,6 +133,7 @@ export default function NewPaletteForm(props) {
                     removeColor={removeColor}
                     axis="xy"
                     onSortEnd={onSortEnd}
+                    distance={20}
                 />
             </main>
         </div>
